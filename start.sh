dcd33ee79513b1bd4f60e99c492766970611993c
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2023-11-11 20:09:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Kushalhk/TG_BOTZ.git /TG_BOTZ 
+  git clone https://github.com/akrcreation2/File_filter.git /File_filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG_BOTZ
+  git clone $UPSTREAM_REPO /File_filter
 fi
 cd /TG_BOTZ 
 pip3 install -U -r requirements.txt
-echo "Starting TG_BOTZ 😎...."
+echo "Starting File_filter 😎...."
 python3 bot.py    
